4bfcd409e94885a378e9b50243a8a50766e0e4f1
Fr4nk1in <74861140+Fr4nk1in-USTC@users.noreply.github.com>
2022-03-23 19:18:57 +0800

    Update report script

--- add_report.ps1
+++ add_report.ps1
@@ -1,2 +1,2 @@
 ${hwnum} = Read-Host "Please input the number of the report: "
-Copy-Item ./report.md ./lab${hwnum}/Lab${hwnum}_PB20000051_傅申.md
+Copy-Item ./report.md ./lab${hwnum}/实验${hwnum}_PB20000051_傅申.md
